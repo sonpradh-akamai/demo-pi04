451d0c14c7ed057554e024761e2cde1f9d5cacc7
Sonit Pradhan <sonpradh@akamai.com>
2023-09-12 13:00:28 +0530

    added sql_db_client

---
+++ sql_db_client.py
@@ -0,0 +1,41 @@
+import pyodbc
+
+server = 'tcp:gcc-mssql-server.database.windows.net'
+database = 'mssql-db'
+username = 'azureuser'
+password = 'Azur3us3r123'
+driver = '{ODBC Driver 18 for SQL Server}'
+
+def create_table(cursor):
+    create_table_query = '''
+    CREATE TABLE sometable(
+                [Key] NVARCHAR(50),
+                    [Value] INT
+                    )
+    '''
+
+    cursor.execute(create_table_query)
+    cursor.commit()
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+'>
+cursor = conn.cursor()
+#create_table(cursor)
+
+insert_query = '''
+INSERT INTO sometable ([Key], [Value])
+VALUES (?, ?)
+'''
+
+data = [('A', 10), ('B', 20), ('C', 30)]
+
+cursor.executemany(insert_query, data)
+cursor.commit() 
+
+cursor.execute("SELECT TOP(1000) * FROM sometable")
+
+for row in cursor:
+        print(row)
+
+
+cursor.close()
+conn.close() 
